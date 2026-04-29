49129823da3690683eae85dfdc2ec9df773006ad
guinhas <tomasthor8@gmail.com>
2026-04-30 00:46:08 +0100

    update search bar to open in a new tab - hopefully works

--- src/js/search_engine.js
+++ src/js/search_engine.js
@@ -43,7 +43,9 @@ document.addEventListener("keydown", (e) => {
 searchInput.addEventListener("keydown", (e) => {
 	if (e.key === "Enter"){
 		const q = searchInput.value.trim();
-		if (q) window.location.href = activeEngine + encodeURIComponent(q);
+		if (q){
+			window.open(activeEngine + encodeURIComponent(q), "_blank", "noopener,noreferrer");
+		}
 	}
 });
 
